40be446a8df1229771c90559408ca1f0d9dadc45
fadytadrous <fadytadrous.ft@gmail.com>
2019-05-03 11:22:43 +0200

    text to vector fixed

--- Fady.hpp
+++ Fady.hpp
@@ -4,25 +4,30 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 #ifndef PROJECT_FADY_HPP
 #define PROJECT_FADY_HPP
 namespace file_read {
     void readLegalWords();
 {
-    std::vector<std::string> wordsArray;
+    td::vector<std::string> wordsArray;
     std::ifstream file;
     file.open ("/home/fady/sbe201-2019-final-project-sbe201-2021-team06/carl_sagan.txt");
-    if (!file) {
+    if (!file)
+    {
     std::cout << "Unable to open file datafile.txt";
     exit(1);   // call system to stop
-}
+    }
+std::string str;
 int index = 0; //counter
-while (std::getline(file, wordsArray[index]))
-{
-file>>wordsArray[index];
-std::cout << wordsArray[index];
-index++;
-file.close(); 
-}
+while (std::getline(file, str))
+    {
+    if(str.size() > 0)
+    wordsArray.push_back(str);
+//std::getline(file, wordsArray[index]);
+    std::cout << wordsArray.front();
+    index++;
+    file.close();
+    }
 }
 #endif //PROJECT_FADY_HPP
